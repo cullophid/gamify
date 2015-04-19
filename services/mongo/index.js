04feb499326cab62bf216335b08dc50c597a3b00
Andreas Møller <andreasmoller@andreas.local>
2015-04-19 21:51:49 +0100

    updated

--- services/mongo/index.js
+++ services/mongo/index.js
@@ -3,6 +3,7 @@ var config = require('config');
 var _ = require('lodash');
 var pmongo = require('promised-mongo');
 
+console.log('MONGODB: ' + config.mongoURI);
 module.exports = pmongo(config.mongoURI);
 module.exports.objectId = pmongo.ObjectId;
 module.exports.utils = require('./utils');

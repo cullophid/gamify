adce8c4c5edbcb4294e6b941b5dc5d603e25669a
Andreas Møller <andreasmoller@andreas.local>
2015-04-19 21:58:37 +0100

    updated npm scripts

--- services/mongo/index.js
+++ services/mongo/index.js
@@ -3,7 +3,7 @@ var config = require('config');
 var _ = require('lodash');
 var pmongo = require('promised-mongo');
 
-console.log('ENV: ' + process.env.NODE_ENV);
+console.log('ENV: ' + process.env);
 console.log('MONGODB: ' + config.mongoURI);
 module.exports = pmongo(config.mongoURI);
 module.exports.objectId = pmongo.ObjectId;
